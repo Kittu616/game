2db89cfebe1d6d209fcca3bc16491e976653d6db
Kshitij R Phutane <121564218+Kittu616@users.noreply.github.com>
2025-02-02 17:27:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1196,6 +1196,7 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on port:3000');
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
